5af2e811b8c13349596800c6082cc9733b72ef29
NaimoonJannat <naimoonjannat@gmail.com>
2024-11-07 18:47:00 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root";
+import Home from "../Components/Home/Home";
+
+const router = createBrowserRouter([ 
+    { 
+        path: "/", 
+        element: <Root />, 
+        // errorElement: ,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>  
+            }
+        ]
+    }
+]);
+export default router;
