4bfac7dd987c0c799821f43b946e0a5656e5a4dc
NaimoonJannat <naimoonjannat@gmail.com>
2024-11-08 13:01:25 +0600

    Loader added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Loader from "../Loader/Loader";
 
 const Home = () => {
     return (
         <div>
             <h1 className="text-5xl">
             ClimateWatch
+            <Loader></Loader>
             </h1>
         </div>
     );
