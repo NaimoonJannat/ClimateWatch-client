f8d5a3968edac679997941db16321a80497abc80
fahmida_fahmi <abdulaziz21298@gmail.com>
2024-11-08 09:05:10 +0600

    add home, project, weather, pagebanner, footer

---
+++ src/Components/Shared/PageBanner/PageBanner.jsx
@@ -0,0 +1,12 @@
+
+const PageBanner = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">
+            ClimateWatch
+            </h1>
+        </div>
+    );
+};
+
+export default PageBanner;
---
+++ src/Components/Weather/Weather.jsx
@@ -0,0 +1,12 @@
+
+const Weather = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">
+            ClimateWatch
+            </h1>
+        </div>
+    );
+};
+
+export default Weather;
