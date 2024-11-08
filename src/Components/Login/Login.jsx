8f370d9b68ab0e522cf475a10c4cb403eda3d9f4
NaimoonJannat <naimoonjannat@gmail.com>
2024-11-08 10:50:34 +0600

    Login page error solved

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const navigate = useNavigate();
 
     // const {signIn, signInGoogle, signInGithub} = useContext(AuthContext);
-    // const {signIn} = useContext(AuthContext);
+    const {signIn} = useContext(AuthContext);
 
    
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import './index.css'
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import AuthProvider from "./Providers/AuthProvider";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render( 
   <React.StrictMode> 
-  <RouterProvider router={router} /> 
+  <AuthProvider><RouterProvider router={router} /></AuthProvider>
   </React.StrictMode> 
  ); 
